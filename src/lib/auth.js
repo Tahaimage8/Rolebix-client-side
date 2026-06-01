9b38e61577d4ab5ae3d941187d6d6b0d2515c7fa
tahaimage8 <tahaimage8@gmail.com>
2026-06-02 03:41:54 +0600

    auto signin fix

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -19,6 +19,7 @@ const db = client.db("role");
 export const auth = betterAuth({
       emailAndPassword: { 
     enabled: true, 
+      autoSignIn: false,
   }, 
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
